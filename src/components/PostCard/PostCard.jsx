ad4bfdc6311e4c88572ae959db52ffde6f418ba5
saurav-ghimire <sauravghimire0123@gmail.com>
2024-03-31 19:23:40 -0400

    Data Fetched From the Mongodb Online Server

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -7,12 +7,14 @@ const PostCard = ({data}) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-      <Image src="/noavatar.png" alt="" height={100} width={200} />
+        {data.img && <div className={styles.imgContainer}>
+          <Image src={data.img} alt="" fill className={styles.img}/>
+        </div>}
         <span className={styles.date}>2016/10/12</span>
       </div>
       <div className={styles.bottom}>
         <h1 className={styles.title}>{data.title}</h1>
-        <p className={styles.desc}>{data.body}</p>
+        <p className={styles.desc}>{data.desc}</p>
         <Link className={styles.link} href={`/blog/${data.id}`}>READ MORE</Link>
       </div>
     </div>
